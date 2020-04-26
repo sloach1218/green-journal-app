857ae52f7acd8a49c5b0c6ad6992e3c4f6e2cbf2
Sarah Loach <Sarah@Sarahs-fat-top.local>
2020-04-26 15:20:04 -0500

    fixed imageModal z-index and updated endpoint urls

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  API_PLANTS_ENDPOINT:'http://localhost:8000/api/plants',
-  /*API_ENDPOINT: 'https://hidden-fjord-85873.herokuapp.com/api',
-  API_PLANTS_ENDPOINT:'https://hidden-fjord-85873.herokuapp.com/api/plants',*/
+  API_ENDPOINT: 'https://hidden-fjord-85873.herokuapp.com/api',
+  API_PLANTS_ENDPOINT:'https://hidden-fjord-85873.herokuapp.com/api/plants',
   TOKEN_KEY: 'green-journal-auth-token',
 }
